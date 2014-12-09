f087af6bf1039e223b7c64617758ec11eafe83a3
Patri <patrimglez@gmail.com>
2014-12-09 10:56:27 +0100

    small fix to name tag

--- lib/onix/representers/header_representer.rb
+++ lib/onix/representers/header_representer.rb
@@ -7,7 +7,7 @@ module ONIX
     self.representation_wrap = :Header
 
     property :sender, as: "Sender", extend: ONIX::SenderRepresenter, class: ONIX::Sender
-    property :send_date_time, as: "SendDateTime", render_filter: ::ONIX::Formatters::YYYYMMDD, parse_filter: ->(value, *context) { Date.parse(value) rescue nil }
+    property :sent_date_time, as: "SentDateTime", render_filter: ::ONIX::Formatters::YYYYMMDD, parse_filter: ->(value, *context) { Date.parse(value) rescue nil }
     property :default_language_of_text, as: "DefaultLanguageOfText"
     property :default_price_type, as: "DefaultPriceType", render_filter: ::ONIX::Formatters::TWO_DIGITS
     property :default_currency_code, as: "DefaultCurrencyCode"
