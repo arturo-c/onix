f2a30c586ee956a6c3bb5d70906a00578fd883aa
Patri <patrimglez@gmail.com>
2014-12-18 13:39:28 +0100

    small fix PriceType instead of PriceIDType

--- lib/onix/representers/price_representer.rb
+++ lib/onix/representers/price_representer.rb
@@ -6,7 +6,7 @@ module ONIX
 
     self.representation_wrap = :Price
 
-    property :price_type, as: "PriceIDType", render_filter: ::ONIX::Formatters::TWO_DIGITS
+    property :price_type, as: "PriceType", render_filter: ::ONIX::Formatters::TWO_DIGITS
     property :price_amount, as: "PriceAmount", render_filter: ::ONIX::Formatters::DECIMAL
     property :currency_code, as: "CurrencyCode"
 
